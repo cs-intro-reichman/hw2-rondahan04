0d7936fb967a928a303722cda8080d66875c5c10
Ron Dahan <rondahan@Rons-MacBook-Pro.local>
2024-11-14 14:58:23 +0200

    Fixed the upper case thing

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,9 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	
+		Double pie = Math.PI;
+		int d = Integer.parseInt(args[0]);
+		
+
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -2,17 +2,17 @@
 public class Cheers {
         public static void main(String[] args) {
 	    String cheers = args[0];
+            cheers=cheers.toUpperCase();
             int times= Integer.parseInt(args[1]);
             int cheersLength = cheers.length();
-            char currentLetter = ' ';
+            char currentLetter;
             int exists;
             String badWords = "AEFHILMNORSX";
             for (int i = 0 ; i < cheersLength; i++){
                 exists = -1;
                 currentLetter = cheers.charAt(i);
-                currentLetter=Character.toUpperCase(currentLetter);
                 exists = badWords.indexOf(currentLetter);
-                  if (exists>=0) {
+                  if (exists >= 0) {
                         System.out.println("Give me an " +  currentLetter +": " +currentLetter + "!");
                 }
                 if (exists == -1){
@@ -20,7 +20,6 @@ public class Cheers {
                 }
             }
             System.out.println("What does that spell?");
-            cheers = cheers.toUpperCase();
             for (int i=0;i<times;i++){
                 System.out.println(cheers + "!!!");
                   }
