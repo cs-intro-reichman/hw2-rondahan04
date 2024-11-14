d47727cbc9c729326d9c420c2b2dc6c2c6dc17d4
Ron Dahan <rondahan@Rons-MacBook-Pro.local>
2024-11-14 14:21:43 +0200

    Finished Cheers

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,9 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		char a = 'A';
+		int x =	"AEFHILMNORSX".indexOf(a);
+		System.out.println(x);
+		
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,7 +1,6 @@
 public class TimeCalc {
     public static void main(String[] args) {
     	int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int minutesToAdd = Integer.parseInt(args[1]);
         int totalMinutes = hours*60+minutes+minutesToAdd;
